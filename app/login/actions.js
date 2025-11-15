024d7885c63d014a0df2301dca15a3ff98a359a4
Alberto Sanchez <ing.albertosanchez@gmail.com>
2025-11-14 22:08:41 -0400

    login server action fixed

--- app/login/actions.js
+++ app/login/actions.js
@@ -12,34 +12,42 @@ export async function loginAction(formData) {
   const user = formData.get("user");
   const password = formData.get("password");
 
-  // Buscar usuario en la DB
-  const found = await prisma.user.findUnique({
-    where: { user },
-  });
+  try {
+    // Buscar usuario en la DB
+    const found = await prisma.user.findUnique({
+      where: { user },
+    });
 
-  if (!found) {
-    return { error: "Usuario no encontrado" };
+    if (!found) {
+      return { error: "Usuario o contraseña incorrecta" };
+    }
+
+    
+    // Comparar contraseña enviada con la encriptada en la base de datos en 
+    // formato hash
+    const ok = await bcrypt.compare(password, found.password);
+    if (!ok) {
+      return { error: "Usuario o contraseña incorrecta" };
+    }
+
+    // cookies() es async en actions
+    const cookieStore = await cookies();
+    // Crear cookie
+    cookieStore.set("session", String(found.id), {
+      httpOnly: true,
+      secure: true,
+      path: "/",
+      maxAge: 60 * 60 * 24 * 30, // 30 días
+    });
+    
+    //return { success: true };
+
+    
+  } catch (error) {
+    return { error: error.message };
   }
-
-  // Comparar contraseña enviada con la encriptada en la base de datos en 
-  // formato hash
-  const ok = await bcrypt.compare(password, found.password);
-  if (!ok) {
-    return { error: "Contraseña incorrecta" };
-  }
-
-  // cookies() es async en actions
-  const cookieStore = await cookies();
-  // Crear cookie
-  cookieStore.set("session", String(found.id), {
-    httpOnly: true,
-    secure: true,
-    path: "/",
-    maxAge: 60 * 60 * 24, // 1 día
-  });
-  
-  //return { success: true };
 
   // Redirigir a dashboard
   redirect("/dashboard");
+
 }
